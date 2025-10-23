96ec0528ed14a8e3d603d5fa1b82d9dc601b2695
pami84 <pamela.s.villalobos.84@gmail.com>
2025-10-23 17:16:32 +0200

    Initial Spring Boot setup

---
+++ src/main/java/ECutbildning/SpringBoot_projekt/SpringBootProjektApplication.java
@@ -0,0 +1,13 @@
+package ECutbildning.SpringBoot_projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ECutbildning/SpringBoot_projekt/SpringBootProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package ECutbildning.SpringBoot_projekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
